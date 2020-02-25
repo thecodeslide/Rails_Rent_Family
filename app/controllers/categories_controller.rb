99ac936837ed077746a8b8493cfff881e8121481
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-02-25 15:50:06 +0900

    first part of category journey

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,32 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [show:, create:]
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(cateogry_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def cateogry_params
+    params.require(:category).permit(:name, :description, :price)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_020146) do
+ActiveRecord::Schema.define(version: 2020_02_25_021951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2020_02_25_020146) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_020146) do
     t.string "gender"
     t.integer "age"
     t.string "personality"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

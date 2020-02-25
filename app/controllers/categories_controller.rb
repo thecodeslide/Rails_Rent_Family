caf8debe9970858ddb2fa09c5e942d93e899efe7
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-02-25 17:44:02 +0900

    finished user journey

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [show:, create:]
+  before_action :set_category, only: [:show, :edit, :update]
   def index
     @categories = Category.all
   end
@@ -12,11 +12,23 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(cateogry_params)
+    @category = Category.new(category_params)
+    @category.user = current_user
     if @category.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to category_path
+    else
+      render :edit
     end
   end
 
@@ -26,7 +38,7 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
-  def cateogry_params
+  def category_params
     params.require(:category).permit(:name, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :category, only: [:show, :index, :new, :create] do
-    resources :booking, only: [:index, :update, :create]
+  resources :categories, only: [:show, :index, :new, :create, :edit, :update] do
+    resources :bookings, only: [:index, :update, :create]
   end
-  resources :user, only: :show
+  resources :users, only: :show
 end

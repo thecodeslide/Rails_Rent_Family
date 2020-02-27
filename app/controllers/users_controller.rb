d3af899785d8872bccbe899271f1bc3462607fe5
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-02-27 12:09:41 +0900

    add_user_location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.geocoded #returns flats with coordinates
-
-    @markers = @flats.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude
-      }
-    end
-  end
-
 
   def my_page
     @categories = current_user.categories

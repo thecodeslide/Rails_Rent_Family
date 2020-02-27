16a48f3788537d9ae0f9975c799f46fe4ed21f1f
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-02-27 14:24:06 +0900

    geocoding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,12 @@ class UsersController < ApplicationController
     @categories = current_user.categories
   end
 
+    @users = User.geocoded #returns flats with coordinates
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
 end
